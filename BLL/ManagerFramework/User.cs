58d6efad55ac228583d56a950e68a0a81a80db3d
wangxu <316729240@qq.com>
2017-01-25 14:46:28 +0800

    222222

---
+++ BLL/ManagerFramework/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UserFramework;
+
+namespace ManagerFramework
+{
+    public class User
+    {
+        public static string Reg(string username,string password,Dictionary<string,object> extendedField)
+        {
+            new Reg(username, password, extendedField);
+            return Login(username,password);
+        }
+        public static void EditPassword(string token,string oldPassword,string newPassword)
+        {
+            
+        }
+        public static string Login(string username,string password)
+        {
+            Login login = new Login(username,password);
+            return login.Token;
+        }
+        public static void Edit(string token, Dictionary<string, object> extendedField)
+        {
+            
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,16 @@
+﻿using ManagerFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login=User.Login("", "");
+        }
+    }
+}
